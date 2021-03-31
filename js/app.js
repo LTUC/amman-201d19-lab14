c59ccc809f5a44b036a36b89106bee7c1bf3c12c
Mohammad Altamimi <m98altamimi@gmail.com>
2021-03-31 16:35:32 +0300

    again

--- js/app.js
+++ js/app.js
@@ -8,10 +8,14 @@ const Cart = function(items) {
 
 Cart.prototype.addItem = function(product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+ 
+  this.items.push(new CartItem(product, quantity));
+
 };
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  localStorage.setItem('cart',JSON.stringify(cart))
 };
 
 Cart.prototype.removeItem = function(item) {
@@ -59,4 +63,3 @@ function generateCatalog() {
 generateCatalog();
 
 
-// im here //
